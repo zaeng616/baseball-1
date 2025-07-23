a77ad7e6afdc0dc1f60d7ec42c2a659d0b780164
jaeyeong.jeon <jyeong.jeon@samsung.com>
2025-07-23 12:59:15 +0900

    add invalid_argument

--- BaseballKATA/baseball.cpp
+++ BaseballKATA/baseball.cpp
@@ -5,5 +5,9 @@ public:
 	void guess(const std::string& string){
 		if (string.length() != 3)
 			throw std::length_error("Must be three letters.");
+		for (auto ch : string) {
+			if (ch < '0' || ch > '9')
+				throw std::invalid_argument("Must be number.");
+		}
 	}
 };
--- BaseballKATA/baseball_test.cpp
+++ BaseballKATA/baseball_test.cpp
@@ -7,6 +7,10 @@ TEST(BaseballGame, ThrowExceptionInputLengthIsUnmatched) {
 	EXPECT_THROW(game.guess(std::string("12")), std::length_error);
 }
 
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(std::string("12s")), std::invalid_argument);
+}
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
